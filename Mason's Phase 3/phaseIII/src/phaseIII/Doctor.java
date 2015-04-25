7b85d06340a7e5e870ab0ce003aa1acab8fa8d12
Mason Denney <masondenney@gmail.com>
2015-04-25 14:19:14 -0700

    Testing Phase 3 project insert

---
+++ Mason's Phase 3/phaseIII/src/phaseIII/Doctor.java
@@ -0,0 +1,33 @@
+package phaseIII;
+import java.util.ArrayList;
+
+public class Doctor {
+	String userID;
+	String password;
+	String name;
+	String officePhoneNumber;
+	String personalPhoneNumber;
+	String hospital;
+	String department;
+	//ArrayList<Patient> patientList = new ArrayList<Patient>();
+	
+	public Doctor(){
+		userID="0";
+		password="password";
+		officePhoneNumber = "555-555-5555";
+		personalPhoneNumber = "555-555-5555";
+		hospital = "Generic Hospital";
+		department = "Generic Department";
+		//patientList = NULL;
+	}
+	
+	void printPatientReport(){
+		System.out.println("THIS IS THE REPORT");
+	}
+	
+	//getSymptom
+	
+	//void patientUrgencySort(ArrayList<patient>){}
+	
+}
+
