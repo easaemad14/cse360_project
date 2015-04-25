b07d0e0eda135d115924b35a7d11327d1ba520e8
Mason Denney <masondenney@gmail.com>
2015-04-25 15:23:55 -0700

    Mason's PatientReport Commit

---
+++ Mason's Phase 3/phaseIII/src/phaseIII/PatientReport.java
@@ -0,0 +1,34 @@
+package phaseIII;
+
+public class PatientReport {
+	int urgencyLevel,nauseaLevel,drowsinessLevel,anxietyLevel,painLevel;
+	String commentString;
+	
+	public PatientReport(Patient a){
+		//
+	}
+	
+	int calculateUrgency(int a,int b,int c,int d,int e){
+		//
+		return 0;
+	}
+	
+	void setSymptomLelvel(){
+		//
+	}
+	
+	void setCommentString(){
+		//
+	}
+	
+	int getSymptomLevel(){
+		//
+		return 0;
+	}
+	
+	String getCommentHistory(){
+		//
+		return "LEL";
+	}
+	
+}
