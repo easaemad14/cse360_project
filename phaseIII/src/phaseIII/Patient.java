080ba01b3b533f201704097a448f208d49dcd3c7
Mason Denney <masondenney@gmail.com>
2015-04-25 10:31:21 -0700

    Patient (incomplete)
    incomplete patient

---
+++ phaseIII/src/phaseIII/Patient.java
@@ -0,0 +1,12 @@
+package phaseIII;
+
+public class Patient {
+	String userID,password,name,address,primaryPhoneNumber,secondaryPhoneNumber,hospital,ailment;
+	Doctor currentDoctor;
+	//ArrayList<PatientReport>
+	int UrgencyLevel,NauseaLevel,DrowsinessLevel,AnxietyLevel,PainLevel,DepressionLevel;
+	
+	Patient(){
+		
+	}
+}
